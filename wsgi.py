4d7d44cfbe13feb970e8c0242857b6d085d74b13
abate2 <deibermoyano517@gmail.com>
2025-08-29 11:38:19 -0500

    Preparando la aplicación para el despliegue

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,4 @@
-from app import create_app
+from app import app as application
 
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    application.run()
